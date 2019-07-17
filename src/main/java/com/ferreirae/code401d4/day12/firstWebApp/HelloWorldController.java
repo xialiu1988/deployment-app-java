942bbbeabe351c7a2c4ef027a93029db6fe5a4ea
xialiu1988 <liuxia198896@gmail.com>
2019-07-17 10:39:14 -0700

    upadted

--- src/main/java/com/ferreirae/code401d4/day12/firstWebApp/HelloWorldController.java
+++ src/main/java/com/ferreirae/code401d4/day12/firstWebApp/HelloWorldController.java
@@ -11,7 +11,7 @@ public class HelloWorldController {
     // specify the route (method and path) for this method
     @GetMapping("/")
     public String getHelloWorld() {
-        return "Hello, world! Today is wednesday hahahhaha ";
+        return "Hello! Today is wednesday hahahhaha ";
     }
 
     @GetMapping("/hello/{name}/{from}")
